01bafd9463bb881a458ae5757417787b4568c528
Pradyumna <pradyu.dp@gmail.com>
2026-01-12 21:40:14 +0530

    table and user profile

---
+++ src/app/tables/basic-table/basic-table.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-basic-table',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './basic-table.html',
+  styleUrl: './basic-table.css',
+})
+export class BasicTableComponent {}
---
+++ src/app/user-profile/user-profile.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-user-profile',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './user-profile.html',
+  styleUrl: './user-profile.css',
+})
+export class UserProfileComponent {}
